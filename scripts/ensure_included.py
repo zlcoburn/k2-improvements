4556881e66d67d81dfd97e04065adbbac4539aba
Jamin W. Collins <jamin.collins@gmail.com>
2025-02-04 15:53:17 -0700

    correct the include required logic
    previous logic would fail if there was not a commented block at the
    bottom of the file
    
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- scripts/ensure_included.py
+++ scripts/ensure_included.py
@@ -5,19 +5,19 @@ import sys
 
 target = f"[include {sys.argv[1]}]"
 
-update_needed = False
+update_needed = True
 
 with open('/mnt/UDISK/printer_data/config/printer.cfg', 'r') as handle:
     contents = handle.readlines()
 
     for line in contents:
         if line.strip() == target:
+            update_needed = False
             break
         if line.startswith('#*#'):
-            update_needed = True
-            contents.insert(contents.index(line), target + '\n')
             break
 
 if update_needed:
+    contents.insert(contents.index(line), target + '\n')
     with open('/mnt/UDISK/printer_data/config/printer.cfg', 'w') as handle:
         handle.writelines(contents)
