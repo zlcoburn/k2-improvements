bcb499493136e11b0e511043e02b805c7aa0d144
Jamin W. Collins <jamin.collins@gmail.com>
2025-02-13 11:05:44 -0700

    ensure custom directory exists
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- features/macros/bed_mesh/install.sh
+++ features/macros/bed_mesh/install.sh
@@ -4,6 +4,8 @@ set -e
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
+test -d ~/printer_data/config/custom || mkdir -p ~/printer_data/config/custom
+
 ln -sf ${SCRIPT_DIR}/bed_mesh.cfg \
     ~/printer_data/config/custom/bed_mesh.cfg
 python ${SCRIPT_DIR}/../../../scripts/ensure_included.py \
--- features/screws_tilt_adjust/install.sh
+++ features/screws_tilt_adjust/install.sh
@@ -6,10 +6,12 @@ SCRIPT_DIR=$(readlink -f $(dirname ${0}))
 ln -sf ${SCRIPT_DIR}/screws_tilt_adjust.py \
     ~/klipper/klippy/extras/screws_tilt_adjust.py
 
+test -d ~/printer_data/config/custom || mkdir -p ~/printer_data/config/custom
+
 ln -sf ${SCRIPT_DIR}/screws_tilt_adjust.cfg \
     ~/printer_data/config/custom/screws_tilt_adjust.cfg
 python ${SCRIPT_DIR}/../../scripts/ensure_included.py \
     ~/printer_data/config/custom/main.cfg \
     screws_tilt_adjust.cfg
 
-#/etc/init.d/klipper restart
+/etc/init.d/klipper restart
