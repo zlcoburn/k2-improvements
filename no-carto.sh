ad340c1fc26bebf45768994c6784a586f9e81379
Jamin W. Collins <jamin.collins@gmail.com>
2025-02-17 11:41:21 -0700

    update no-carto option with current feature list
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- no-carto.sh
+++ no-carto.sh
@@ -12,10 +12,14 @@ install_feature() {
     fi
 }
 
+install_feature better-init
+install_feature skip-setup
 install_feature moonraker
 install_feature fluidd
 install_feature screws_tilt_adjust
+#install_feature cartographer
 mkdir -p /tmp/macros
 install_feature macros/bed_mesh
 install_feature macros/m191
 install_feature macros/start_print
+install_feature macros/overrides
