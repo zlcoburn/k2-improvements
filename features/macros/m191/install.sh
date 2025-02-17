c53319aa46adc32d3e7b5d87e732c10d6e2e6474
Jamin W. Collins <jamin.collins@gmail.com>
2025-02-17 08:09:49 -0700

    ensure the custom/main.cfg is included
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- features/macros/m191/install.sh
+++ features/macros/m191/install.sh
@@ -6,6 +6,10 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
 test -d ~/printer_data/config/custom || mkdir -p ~/printer_data/config/custom
 
+# add the main.cfg to printer.cfg
+python ${SCRIPT_DIR}/../../scripts/ensure_included.py \
+    ~/printer_data/config/printer.cfg custom/main.cfg
+# add the m191.cfg
 ln -sf ${SCRIPT_DIR}/m191.cfg \
     ~/printer_data/config/custom/m191.cfg
 python ${SCRIPT_DIR}/../../../scripts/ensure_included.py \
--- features/macros/start_print/install.sh
+++ features/macros/start_print/install.sh
@@ -6,6 +6,10 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
 test -d ~/printer_data/config/custom || mkdir -p ~/printer_data/config/custom
 
+# add the main.cfg to printer.cfg
+python ${SCRIPT_DIR}/../../scripts/ensure_included.py \
+    ~/printer_data/config/printer.cfg custom/main.cfg
+# add the start_print.cfg
 ln -sf ${SCRIPT_DIR}/start_print.cfg \
     ~/printer_data/config/custom/start_print.cfg
 python ${SCRIPT_DIR}/../../../scripts/ensure_included.py \
